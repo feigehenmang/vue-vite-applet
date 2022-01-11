87f4c1abb5bcd748d77669e7b6d61779f7c287ca
lijinfei <1638546595@qq.com>
2022-01-11 20:41:01 +0800

    add gz

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import viteCompression from 'vite-plugin-compression'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), viteCompression({
+    verbose: true,
+    disable: false,
+    threshold: 10240,
+    algorithm: 'gzip',
+    ext: '.gz',
+  })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
